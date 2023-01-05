d5f8c37407408bf0de3a6090074e176d5ef4b6af
Hitarth Mistry <hitarthmistry42@gmail.com>
2023-01-04 20:23:04 -0600

    Added SignIn Page and Control Extension classes

--- invenauto.team.metal/src/test/java/pages/HomePage.java
+++ invenauto.team.metal/src/test/java/pages/HomePage.java
@@ -1,5 +1,6 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class HomePage extends PageObject{
@@ -11,5 +12,12 @@ public class HomePage extends PageObject{
 	public HomePage navigate() {
 		this.driver.get("https://invenauto.tech/index.php");
 		return this;
+	}
+	
+	public SignInPage clickSignInLink() {
+		var menuLink = this.driver.findElement(By.cssSelector("a[class='login']"));
+		menuLink.click();	
+		
+		return new SignInPage(this.driver);
 	}
 }
