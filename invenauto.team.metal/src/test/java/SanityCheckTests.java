aff990f9b68960c86eb27008c9a27a181f41225b
RogueLament <45111295+RogueLament@users.noreply.github.com>
2023-01-04 17:24:10 -0800

    Added foundation folder and changed SanityCheckTests to SanityTests

--- invenauto.team.metal/src/test/java/SanityCheckTests.java
+++
@@ -1,7 +0,0 @@
-import org.testng.annotations.Test;
-
-public class SanityCheckTests {
-  @Test
-  public void f() {
-  }
-}
---
+++ invenauto.team.metal/src/test/java/SanityTests.java
@@ -0,0 +1,25 @@
+import static org.testng.Assert.assertTrue;
+
+import org.testng.annotations.Test;
+
+import foundation.DriverManagerFactory;
+import foundation.TestBase;
+
+public class SanityTests extends TestBase {
+	@Test
+	public void canUseVar() {
+		String myString = "Taylo  rDennis";
+		var myVarString = "Dennis";			  
+	}
+	
+	@Test
+	public void canLaunchChromeDriver() {
+		var browserType = "chrome";
+		
+		this.manager = DriverManagerFactory.getManager(browserType);
+		this.manager.createDriver();
+		this.driver = this.manager.getDriver();
+		
+		assertTrue(this.driver != null, "the driver should be launched");
+	}
+}
