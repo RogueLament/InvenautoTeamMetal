788bcb992ba41b313c28872323c48e50017a3d33
Trevor John Kleinstuber <trevor.kleinstuber@gmail.com>
2023-01-14 13:20:33 -0800

    jiggles mouse

--- invenauto.team.metal/src/test/java/pages/TShirtPage.java
+++ invenauto.team.metal/src/test/java/pages/TShirtPage.java
@@ -1,6 +1,7 @@
 package pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
 
 public class TShirtPage extends PageObject
 {
@@ -12,6 +13,9 @@ public class TShirtPage extends PageObject
 	public ProductDetailPage clickonfadedshortsleeveShirt() {
 		var productContainer = getProductContainerWithDescription("T-shirt");
 		mouseover(productContainer);
+		new Actions(driver)
+        .moveByOffset(-10, -10)
+        .perform();
 		productContainer.clickMore();
 
 		return new ProductDetailPage(this.driver);
